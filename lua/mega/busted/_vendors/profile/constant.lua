ec531b385cc609ede98390b389f81b3f9d255f4b
Colin Kennedy <colinvfx@gmail.com>
2025-01-04 07:06:08 -0800

    Added profile/constant.lua

---
+++ lua/mega/busted/_vendors/profile/constant.lua
@@ -0,0 +1,12 @@
+--- Useful symbolic variables that other modules use (instead of hard-coding).
+---
+---@module 'mega.busted._vendors.profile.constant'
+---
+
+local M = {}
+
+M.Category = {
+  ["function"] = "function",
+}
+
+return M
